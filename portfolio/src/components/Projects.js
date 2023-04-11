5c3fccd8309396d223188fa2508c386c3f1f38f3
Joshua Owusu <joshbillion1@gmail.com>
2023-04-11 16:35:15 +0000

    Commit auto_update

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -3,6 +3,7 @@ import { ProjectCard } from "./ProjectCard";
 import projects1 from "../assets/img/projects1.png";
 import projects2 from "../assets/img/projects2.png";
 import projects3 from "../assets/img/projects3.png";
+import projects4 from "../assets/img/projects4.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
