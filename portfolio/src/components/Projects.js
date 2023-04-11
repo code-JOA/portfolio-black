ea60882dfd562bf236a4feaa64a53313752a10a9
Joshua Owusu <joshbillion1@gmail.com>
2023-04-11 16:54:29 +0000

    Commit auto_update

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -4,6 +4,7 @@ import projects1 from "../assets/img/projects1.png";
 import projects2 from "../assets/img/projects2.png";
 import projects3 from "../assets/img/projects3.png";
 import projects4 from "../assets/img/projects4.png";
+import projects5 from "../assets/img/projects4.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -34,7 +35,7 @@ export const Projects = () => {
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projects2,
+      imgUrl: projects5,
     },
     {
       title: "Business Startup",
